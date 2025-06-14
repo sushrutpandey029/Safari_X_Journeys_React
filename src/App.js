7a89a0936c16f7e8771170546521fe2524c14c5c
dev <dev@RAJNISH.local>
2025-06-14 18:26:31 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import logo from './logo.svg';
-import HOME from './coponent/Home/home';
+import Home from './coponent/Home/home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import Header from './coponent/Master/header';
+import Footer from './coponent/Master/fotter';
+
 import Booking from './coponent/Booking/Booking';
+import Places from './coponent/places/Places';
+
+
 
 function App() {
   return (
     <div>
-      { <HOME />}
+    <Header />
+    {/* <Home/> */}
       <Booking/>
+      <Places/>
+      <Footer />
     </div>
   );
 }
